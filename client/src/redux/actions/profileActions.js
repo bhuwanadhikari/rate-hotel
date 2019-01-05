8820f558cff4a3fc5fa323d2de9006224c56887e
BhuwanAdhikari <adhikaribhuwan97@gmail.com>
2019-01-05 23:39:21 +0545

    Prepare to deploy

--- client/src/redux/actions/profileActions.js
+++ client/src/redux/actions/profileActions.js
@@ -19,7 +19,7 @@ export const getCurrentProfile = () => dispatch => {
 
          dispatch({
                type: GET_PROFILE,
-               payload: convertProfileObject(res.data)
+               payload: res.data
 
          }
          )}
@@ -62,11 +62,11 @@ export const editCurrentProfile = (profileData) => (dispatch) => {
       .then((res) => {
          dispatch({
             type: GET_PROFILE,
-            payload: convertProfileObject(res.data)
+            payload: res.data
          });
          dispatch({
             type: GET_ERRORS,
-            payload: {msg: 'cleared'} //just for the errors to be cleared
+            payload: {} //just for the errors to be cleared
          });
       })
       .catch((err)=> dispatch({
