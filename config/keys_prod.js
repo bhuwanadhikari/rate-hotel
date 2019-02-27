098112f36d3550366e4a850226455dd2856598ac
BhuwanAdhikari <adhikaribhuwan97@gmail.com>
2019-02-28 00:18:35 +0545

    Test keys for deployment

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,9 @@
 module.exports = {
-   mongoURL: process.env.MONGO_URI,
-   secret: process.env.SECRET_OR_KEY
+
+   // mongoURL: process.env.MONGO_URI,
+   mongoURL: "mongodb://bhuwan:admin1234@ds163156.mlab.com:63156/rating-app",
+
+   // secret: process.env.SECRET_OR_KEY
+   secret: 'secret'
+
 };
